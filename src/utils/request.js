b7fea6f20b8fcc97452b2e40c12746a72abdb8d0
jgoriginal <932274204@qq.com>
2022-08-11 23:07:09 +0800

    登录接口

--- src/utils/request.js
+++ src/utils/request.js
@@ -85,7 +85,7 @@
 // export default service
 
 import axios from 'axios'
-const service = axios.create() // 创建一个axios的实例
+const service = axios.create()   // 创建一个axios的实例
 service.interceptors.request.use() // 请求拦截器
 service.interceptors.response.use() // 响应拦截器
 export default service // 导出axios实例
